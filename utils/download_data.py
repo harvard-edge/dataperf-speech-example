dfc53de5f3f4a5cb17c07f5afda604e7564c3480
David Jurado <david.jurado@factored.ai>
2022-06-28 10:15:22 -0500

    Update MLCube pipeline

--- utils/download_data.py
+++ utils/download_data.py
@@ -3,16 +3,27 @@ import argparse
 import os
 import yaml
 import wget
+import gdown
 import tarfile
+import zipfile
 
 
-def download_file(url, folder_path, extract=False):
+def download_file(url, folder_path, extract=False, g_drive=False):
     """Download file from internet"""
-    output_path = wget.download(url, out=folder_path)
-    if extract:
-        tar = tarfile.open(output_path, "r:gz")
-        tar.extractall(folder_path)
-        tar.close()
+    if url:
+        if g_drive:
+            file_name = "preliminary_evaluation_dataset.zip"
+            output_path = os.path.join(folder_path, file_name)
+            gdown.download(url, output_path, quiet=False, fuzzy=True)
+            if extract:
+                with zipfile.ZipFile(output_path, 'r') as zip_ref:
+                    zip_ref.extractall(folder_path)
+        else:
+            output_path = wget.download(url, out=folder_path)
+            if extract:
+                tar = tarfile.open(output_path, "r:gz")
+                tar.extractall(folder_path)
+                tar.close()
 
 
 def main():
@@ -34,13 +45,12 @@ def main():
         params = yaml.full_load(f)
 
     output_path = args.output_path
-    dataset_url = params["dataset_url"]
-    metadata_url = params["metadata_url"]
-    embeddings_url = params["embeddings_url"]
-
-    download_file(metadata_url, output_path)
-    download_file(dataset_url, output_path, extract=True)
-    download_file(embeddings_url, output_path, extract=True)
+    if "dataset_url" in params:
+        download_file(params["dataset_url"], output_path, extract=True)
+    if "metadata_url" in params:
+        download_file(params["metadata_url"], output_path)
+    if "embeddings_url" in params:
+        download_file(params["embeddings_url"], output_path, extract=True, g_drive=True)
 
 
 if __name__ == "__main__":
